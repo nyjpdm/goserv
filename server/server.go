b7ca2db8e19f71463cf29859415ec57090c990e2
Евгений Шафеев <shafeev.ev@yandex.ru>
2025-12-05 03:43:16 +0300

    poka tak sebe no rabotaet

--- server/server.go
+++ server/server.go
@@ -7,21 +7,24 @@ import (
 	"net/http"
 	"path/filepath"
 	"runtime"
+	"strconv"
 
 	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
 )
 
 type User struct {
-	Name   string `json:"name"`
-	Status string `json:"status"`
+	Name   string          `json:"name"`
+	Status string          `json:"status"`
+	Conn   *websocket.Conn `json:"-"`
 }
 type GameRoom struct {
-	board       GoTree
-	whitePlayer User
-	blackPlayer User
-	watching    []User
+	board       *GoTree
+	whitePlayer *User
+	blackPlayer *User
+	watching    []*User
 	Status      string
+	Name        string
 }
 type MoveQuery struct {
 	Player     string `json:"player"`
@@ -37,22 +40,24 @@ type ApiRequest struct {
 }
 
 type Server struct {
-	players []User
-	games   []GameRoom
+	players []*User
+	games   []*GameRoom
 }
 
-var ourgame = NewGoTree(GameSettings{BoardSize: 9})
+var ourServer Server
 
-func applyMoveQuery(request ApiRequest, tree GoTree) error {
-	if request.Move.X < -2 || request.Move.X > ourgame.BoardSize-1 || request.Move.Y < 0 || request.Move.Y > ourgame.BoardSize-1 {
+//var ourgame = NewGoTree(GameSettings{BoardSize: 9})
+
+func applyMoveQuery(request ApiRequest, tree *GoTree) error {
+	if request.Move.X < -2 || request.Move.X > tree.BoardSize-1 || request.Move.Y < 0 || request.Move.Y > tree.BoardSize-1 {
 		//	http.Error(w, `{"error": "move outside bounds"}`, http.StatusBadRequest)
 		return nil
 	}
 	if request.Action == "clear board" {
-		ourgame = NewGoTree(GameSettings{})
+		tree = NewGoTree(GameSettings{})
 		return nil
 	} else {
-		move_result := ourgame.MakeMove(request.Move.Y*ourgame.BoardSize + request.Move.X)
+		move_result := tree.MakeMove(request.Move.Y*tree.BoardSize + request.Move.X)
 		return move_result
 	}
 
@@ -113,11 +118,14 @@ func runServer() {
 	// Регистрируем обработчики
 
 	r.HandleFunc("/", homeHandler)
-	//r.HandleFunc("/api", apiHandler)
 	r.HandleFunc("/ws", handleWebSocket)
 	fmt.Println("Сервер запущен на http://localhost:8080")
 	fmt.Println("Структура проекта:")
 	fmt.Printf("Корень: %s\n", getRootPath())
+	var firstRoom = GameRoom{Name: "our Game", Status: "ongoing", blackPlayer: nil, whitePlayer: nil}
+	firstRoom.board = NewGoTree(GameSettings{BoardSize: 9})
+	ourServer = Server{games: []*GameRoom{}, players: []*User{}}
+	ourServer.games = append(ourServer.games, &firstRoom)
 	http.ListenAndServe(":8080", r)
 }
 
@@ -127,6 +135,35 @@ func handleWebSocket(w http.ResponseWriter, r *http.Request) {
 		log.Println("WebSocket upgrade failed:", err)
 		return
 	}
+	thisPlayer := User{}
+	thisPlayer.Conn = conn
+	thisPlayer.Name = "player_" + strconv.Itoa(len(ourServer.players))
+	ourServer.players = append(ourServer.players, &thisPlayer)
+	fmt.Println("number of players:", len(ourServer.players))
+	var playColor PointColor //создаем игрока (пока при каждом новом обращении к вебсокету)
+	if ourServer.games[0].blackPlayer == nil {
+		ourServer.games[0].blackPlayer = &thisPlayer
+		playColor = black
+	} else if ourServer.games[0].whitePlayer == nil {
+		ourServer.games[0].whitePlayer = &thisPlayer
+		playColor = white
+	}
+	mess := map[string]interface{}{ //отправляем данные игрока, которые (пока) создает сервер
+		"status":       "ok",
+		"msg":          "your username",
+		"playingColor": playColor,
+		"username":     thisPlayer.Name,
+		"boardState":   stringBoard(ourServer.games[0].board),
+	}
+	messg, _ := json.Marshal(mess)
+	conn.WriteMessage(websocket.TextMessage, messg)
+
+	go handleClient(conn, thisPlayer)
+
+}
+
+func handleClient(conn *websocket.Conn, thisPlayer User) {
+
 	defer conn.Close()
 
 	for {
@@ -144,23 +181,46 @@ func handleWebSocket(w http.ResponseWriter, r *http.Request) {
 			continue
 		}
 		fmt.Println(request.Move)
-		res := applyMoveQuery(request, *ourgame)
+		res := applyMoveQuery(request, ourServer.games[0].board)
 		fmt.Println(res)
 
 		fmt.Println("Отправляем на клиент:")
-		fmt.Print(stringBoard(ourgame))
-		fmt.Println(string(ourgame.CurrentNode.LastMoveColor.Opposite()))
-		response := map[string]interface{}{
-			"status":       "ok",
-			"msg":          "json received",
-			"accepted":     res == nil,
-			"boardState":   stringBoard(ourgame), // возвращаем клиенту
-			"playingColor": string(ourgame.CurrentNode.LastMoveColor.Opposite()),
-			"blackScore":   ourgame.CurrentNode.BlackCaptures,
-			"whiteScore":   ourgame.CurrentNode.WhiteCaptures,
+		fmt.Print(stringBoard(ourServer.games[0].board))
+		fmt.Println(string(ourServer.games[0].board.CurrentNode.LastMoveColor.Opposite()))
+		if ourServer.games[0].blackPlayer != nil {
+			sendBoard(ourServer.games[0].board, ourServer.games[0].board.CurrentNode.LastMoveColor.Opposite(), ourServer.games[0].blackPlayer.Conn, res == nil)
 		}
-		resmess, _ := json.Marshal(response)
-		conn.WriteMessage(websocket.TextMessage, resmess)
+		if ourServer.games[0].whitePlayer != nil {
+			sendBoard(ourServer.games[0].board, ourServer.games[0].board.CurrentNode.LastMoveColor, ourServer.games[0].whitePlayer.Conn, res == nil)
+		}
+
+		sendBoard(ourServer.games[0].board, ourServer.games[0].board.CurrentNode.LastMoveColor.Opposite(), conn, res == nil)
+		// response := map[string]interface{}{
+		// 	"status":       "ok",
+		// 	"msg":          "updating board",
+		// 	"accepted":     res == nil,
+		// 	"boardState":   stringBoard(ourgame.board), // возвращаем клиенту
+		// 	"playingColor": string(ourgame.board.CurrentNode.LastMoveColor.Opposite()),
+		// 	"blackScore":   ourgame.board.CurrentNode.BlackCaptures,
+		// 	"whiteScore":   ourgame.board.CurrentNode.WhiteCaptures,
+		// }
+		// resmess, _ := json.Marshal(response)
+		// conn.WriteMessage(websocket.TextMessage, resmess)
 
 	}
+
+}
+
+func sendBoard(board *GoTree, color PointColor, conn *websocket.Conn, accepted bool) {
+	response := map[string]interface{}{
+		"status":       "ok",
+		"msg":          "updating board",
+		"accepted":     accepted,
+		"boardState":   stringBoard(board), // возвращаем клиенту
+		"playingColor": color,
+		"blackScore":   board.CurrentNode.BlackCaptures,
+		"whiteScore":   board.CurrentNode.WhiteCaptures,
+	}
+	resmess, _ := json.Marshal(response)
+	conn.WriteMessage(websocket.TextMessage, resmess)
 }
